ae099b5067110ad4b61b305d2fb0e6b697f2f7bf
Rickey <rickey@Rickeys-MacBook-Pro.local>
2017-05-04 13:44:53 -0700

    Completed ch18 testing
    Except for exercises

---
+++ django_ecommerce/payments/migrations/0009_auto_20170504_1216.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('payments', '0008_auto_20170425_2001'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='unpaidusers',
+            name='last_notification',
+            field=models.DateTimeField(default=datetime.datetime(2017, 5, 4, 12, 16, 59, 343779)),
+        ),
+    ]
