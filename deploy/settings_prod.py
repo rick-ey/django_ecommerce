054881bc9c1e8e46536259c99d50b3d7ae1b6448
Rickey <rickey@Rickeys-MacBook-Pro.local>
2017-05-05 12:25:37 -0700

    Completed ch19
    except the exercises.

---
+++ deploy/settings_prod.py
@@ -0,0 +1,20 @@
+# turn off debugging in production
+DEBUG = False
+
+# settings for the production database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_db',
+        'USER': 'djangousr',
+        'PASSWORD': 'gq6877',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+# allowed hosts for our production site
+ALLOWED_HOSTS = ['162.243.159.92/']
+
+STATIC_ROOT = '/opt/mec_env/static/'
+MEDIA_ROOT = '/opt/mec_env/media/'
